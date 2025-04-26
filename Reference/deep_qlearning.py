1fae8917a34c04429580e4b0915b5f839174de66
Meru:3 <maulanaammar101@gmail.com>
2025-04-26 13:37:45 +0700

    add agent modification

--- Reference/deep_qlearning.py
+++ Reference/deep_qlearning.py
@@ -2,7 +2,28 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Activation, Flatten
 from keras.callbacks import TensorBoard
 from keras.optimizers import Adam
+from collections import deque
 
+import time
+
+REPLAY_MEMORY_SIZE = 50_000
+MODEL_NAME = "256x2"
+
+class ModifiedTensorBoard(TensorBoard):
+    # override init step and writer
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.step = 1
+        self.writer = tf.summary.FileWriter(self.log_dir)
+    
+    # override method to stop creating default log writer
+    def set_model(self, model):
+        pass
+
+    # override, save logs with our step number otherwise every.fit will start from 0 step
+    def on_epoch_end(self, epoch, logs=None):
+        self.update_stats(**logs)
+    
 
 class DQNAgent:
     def __init__(self):
@@ -13,6 +34,10 @@ class DQNAgent:
         # target model
         self.target_model = self.create_model()
         self.target_model.set_weights(self.model.get_weights())
+
+        self.replay_memory = deque(maxLen=REPLAY_MEMORY_SIZE)
+        self.tensorboard = ModifiedTensorBoard(Log_dir=f"logs/{MODEL_NAME}-{int(time.time())}")
+        self.target_update_counter = 0
 
     def create_model(self):
         model = Sequential()
